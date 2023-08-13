2e272cf30326e8bf220f5aac0f6dde7f0d9acd16
Siddharth Saha <sisahawork@gmail.com>
2023-08-13 12:00:32 -0400

    Adding master pos and rover pos functionality

--- src/novatel_oem7_driver/src/align_handler.cpp
+++ src/novatel_oem7_driver/src/align_handler.cpp
@@ -29,6 +29,8 @@
 
 #include <novatel_oem7_driver/oem7_ros_messages.hpp>
 #include <novatel_oem7_msgs/msg/heading2.hpp>
+#include "novatel_oem7_msgs/msg/masterpos.hpp"
+#include "novatel_oem7_msgs/msg/roverpos.hpp"
 
 #include <oem7_ros_publisher.hpp>
 
@@ -42,6 +44,8 @@ namespace novatel_oem7_driver
   class ALIGNHandler: public Oem7MessageHandlerIf
   {
     std::unique_ptr<Oem7RosPublisher<novatel_oem7_msgs::msg::HEADING2>> HEADING2_pub_; ///< Publisher for NMEA sentences;
+    std::unique_ptr<Oem7RosPublisher<novatel_oem7_msgs::msg::MASTERPOS>> MASTERPOS_pub_;
+    std::unique_ptr<Oem7RosPublisher<novatel_oem7_msgs::msg::ROVERPOS>> ROVERPOS_pub_;
 
 
     void publishHEADING2(
@@ -50,6 +54,22 @@ namespace novatel_oem7_driver
       std::shared_ptr<novatel_oem7_msgs::msg::HEADING2> heading2;
       MakeROSMessage(msg, heading2);
       HEADING2_pub_->publish(heading2);
+    }
+
+    void publishMASTERPOS(
+        Oem7RawMessageIf::ConstPtr msg)
+    {
+      std::shared_ptr<novatel_oem7_msgs::msg::MASTERPOS> masterpos;
+      MakeROSMessage(msg, masterpos);
+      MASTERPOS_pub_->publish(masterpos);
+    }
+
+    void publishROVERPOS(
+        Oem7RawMessageIf::ConstPtr msg)
+    {
+      std::shared_ptr<novatel_oem7_msgs::msg::ROVERPOS> roverpos;
+      MakeROSMessage(msg, roverpos);
+      ROVERPOS_pub_->publish(roverpos);
     }
 
   public:
@@ -64,17 +84,39 @@ namespace novatel_oem7_driver
     void initialize(rclcpp::Node& node)
     {
       HEADING2_pub_ = std::make_unique<Oem7RosPublisher<novatel_oem7_msgs::msg::HEADING2>>("HEADING2", node);
+      MASTERPOS_pub_ = std::make_unique<Oem7RosPublisher<novatel_oem7_msgs::msg::MASTERPOS>>("MASTERPOS", node);
+      ROVERPOS_pub_ = std::make_unique<Oem7RosPublisher<novatel_oem7_msgs::msg::ROVERPOS>>("ROVERPOS", node);
     }
 
     const MessageIdRecords& getMessageIds()
     {
-      static const MessageIdRecords MSG_IDS({{HEADING2_OEM7_MSGID, MSGFLAG_NONE}});
+      static const MessageIdRecords MSG_IDS({
+        {HEADING2_OEM7_MSGID, MSGFLAG_NONE},
+        {MASTERPOS_OEM7_MSGID, MSGFLAG_NONE},
+        {ROVERPOS_OEM7_MSGID, MSGFLAG_NONE},
+      });
       return MSG_IDS;
     }
 
     void handleMsg(Oem7RawMessageIf::ConstPtr msg)
     {
-      publishHEADING2(msg);
+      if(msg->getMessageId()== HEADING2_OEM7_MSGID)
+      {
+        publishHEADING2(msg);
+      }
+      else if (msg->getMessageId()== MASTERPOS_OEM7_MSGID)
+      {
+        publishMASTERPOS(msg);
+      }
+      else if (msg->getMessageId()== ROVERPOS_OEM7_MSGID)
+      {
+        publishROVERPOS(msg);
+      }
+      else
+      {
+        assert(false);
+      }
+      
     }
   };
 }
