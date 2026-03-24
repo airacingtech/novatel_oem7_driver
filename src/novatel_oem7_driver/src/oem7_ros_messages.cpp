7fd780e19c1f0770679a1ae7537b4b725ed43687
Kevin Shin <kevshin2002@gmail.com>
2026-03-24 06:28:41 -1000

    Assertions wrong msg ID

--- src/novatel_oem7_driver/src/oem7_ros_messages.cpp
+++ src/novatel_oem7_driver/src/oem7_ros_messages.cpp
@@ -130,7 +130,7 @@ MakeROSMessage<novatel_oem7_msgs::msg::BESTPOS>(
     const Oem7RawMessageIf::ConstPtr& msg,
     std::shared_ptr<novatel_oem7_msgs::msg::BESTPOS>& bestpos)
 {
-  assert(msg->getMessageId() == BESTGNSSPOS_OEM7_MSGID);
+  assert(msg->getMessageId() == BESTPOS_OEM7_MSGID);
 
   const BESTPOSMem* bp = reinterpret_cast<const BESTPOSMem*>(msg->getMessageData(OEM7_BINARY_MSG_HDR_LEN));
   bestpos.reset(new novatel_oem7_msgs::msg::BESTPOS);
@@ -167,7 +167,7 @@ MakeROSMessage<novatel_oem7_msgs::msg::BESTVEL>(
     const Oem7RawMessageIf::ConstPtr& msg,
     std::shared_ptr<novatel_oem7_msgs::msg::BESTVEL>& bestvel)
 {
-  assert(msg->getMessageId() == BESTGNSSVEL_OEM7_MSGID);
+  assert(msg->getMessageId() == BESTVEL_OEM7_MSGID);
 
   const BESTVELMem* bv = reinterpret_cast<const BESTVELMem*>(msg->getMessageData(OEM7_BINARY_MSG_HDR_LEN));
   bestvel.reset(new novatel_oem7_msgs::msg::BESTVEL);
